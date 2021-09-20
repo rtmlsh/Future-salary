d1af247eb508e0c7088495bcbcb2b5ee4c76aca1
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-09-20 13:27:21 +0300

    Отсортировал по региону и дате публикации

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import pprint
 
 url = 'https://api.hh.ru/vacancies/'
-payload = {'text': 'Программист', }
+payload = {'text': 'Программист', 'area': 1, 'period': 20}
 response = requests.get(url, params=payload)
 response.raise_for_status()
 pprint.pprint(response.json())
