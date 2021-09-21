9e65f429f15c6a6186ea6784e2907489f97af2de
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-09-21 23:46:33 +0300

    Сделал статистику вакансий

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import requests
 import pprint
 
 url = 'https://api.hh.ru/vacancies/'
-payload = {'text': 'Программист', 'area': 1, 'period': 20}
-response = requests.get(url, params=payload)
-response.raise_for_status()
-pprint.pprint(response.json())
+vacancies = {'Python', 'Java', 'Javascript', 'Go', 'Scala', 'Ruby', 'C++', 'PHP'}
+vacancies_statistics = {}
+
+def search_job(vacancy):
+    payload = {'text': f'Программист {vacancy}', 'area': 1, 'period': 30, 'only_with_salary': 'True'}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()['found']
+
+
+for vacancy in vacancies:
+    count_vacancies = search_job(vacancy)
+    job_name = vacancy
+    vacancies_statistics[job_name] = count_vacancies
+
+
+pprint.pprint(vacancies_statistics)
+
