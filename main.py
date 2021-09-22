15c9e987d44833094829ea49a07422860b04cfa4
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-09-22 11:28:09 +0300

    Оформил вызов функций

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import requests
 import pprint
 
 url = 'https://api.hh.ru/vacancies/'
-vacancies = {'Python', 'Java', 'Javascript', 'Go', 'Scala', 'Ruby', 'C++', 'PHP'}
-vacancies_statistics = {}
 
-def search_job(vacancy):
+def search_job(vacancy, url):
     payload = {'text': f'Программист {vacancy}', 'area': 1, 'period': 30, 'only_with_salary': 'True'}
     response = requests.get(url, params=payload)
     response.raise_for_status()
     return response.json()['found']
 
+def give_statistics(url):
+    vacancies = {'Python', 'Java', 'Javascript', 'Go', 'Scala', 'Ruby', 'C++', 'PHP'}
+    vacancies_statistics = {}
+    for vacancy in vacancies:
+        count_vacancies = search_job(vacancy, url)
+        job_title = vacancy
+        vacancies_statistics[job_title] = count_vacancies
+    pprint.pprint(vacancies_statistics)
 
-for vacancy in vacancies:
-    count_vacancies = search_job(vacancy)
-    job_title = vacancy
-    vacancies_statistics[job_title] = count_vacancies
-
-
-pprint.pprint(vacancies_statistics)
-
+give_statistics(url)
