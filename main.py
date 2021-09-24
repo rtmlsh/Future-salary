9a0c0e1a9cf2a47460fc74812e76cdfa97bf8f2d
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-09-25 01:00:01 +0300

    Сделал запрос к API Superjob с выводом нужных профессий

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pprint
 superjob_api = 'v3.r.124446881.cc370bafaaee6098c089f335a7902bc241c1bc02.28bcb2cd6a9e7b9d79fd7930896a81a00d847dd6'
 url = 'https://api.superjob.ru/2.0/vacancies/'
 
-payload = {'X-Api-App-Id': superjob_api}
-response = requests.get(url, headers=payload)
+header = {'X-Api-App-Id': superjob_api}
+payload = {'catalogues': 33, 'keyword': 'Программист', 'town': 4}
+response = requests.get(url, headers=header, params=payload)
 response.raise_for_status()
-print(response.url)
 for profession in response.json()['objects']:
-    print(profession['profession'])
+    print(profession['profession'], profession['town']['title'])
