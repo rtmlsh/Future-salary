2f1a6bea36d253d41bf4e0216ee9110ea7f0ae8f
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-10-12 21:21:12 +0300

    Убрал сравнения с нулём

--- count_average_salaries.py
+++ count_average_salaries.py
@@ -2,7 +2,7 @@ import numpy
 
 
 def predict_salary(payment_from, payment_to, predicted_salaries):
-    if payment_from == 0 and payment_to == 0:
+    if not payment_from and not payment_to:
         None
     elif payment_from and payment_to:
         predicted_salaries.append(numpy.mean([payment_from, payment_to]))
